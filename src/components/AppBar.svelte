7cf8c64f916ba738b721a8ca138a465dcc98190b
Tom Konink <konink1992@gmail.com>
2020-09-26 17:48:16 +0200

    first commit

---
+++ src/components/AppBar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    interface AppBarProps {
+        height: string;
+        top: string;
+    }
+    export let props: AppBarProps
+</script>
+
+<style>
+    header{
+        height: var(--height);
+        top: var(--top);
+        width: 100%;
+        background-color: black;
+        color:white;
+        position: fixed;
+        z-index: 1200;
+    }
+</style>
+
+<header style="--height:{props.height};--top:{props.top}">
+    Let's test this.
+</header>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Nav from '../components/Nav.svelte';
+	import Appbar from '../components/AppBar.svelte';
+	export let segment: string;
+	let myObj = { height: "56px", top: "200px" };
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Appbar props={myObj}/>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
